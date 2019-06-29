9868955dd5ba33c1ca20f4b674e1a63635b2ddf6
xmeng <mxf3306@gmail.com>
2019-06-29 20:38:51 +0100

    1000 examples and 20 epochs, the result is perfect

--- parity_code.py
+++ parity_code.py
@@ -25,7 +25,7 @@ def compute_parity_seq(seq):
     
     return parity_li
 
-seq_li = [torch.LongTensor(generate_binary_seq()) for _ in range(100)]
+seq_li = [torch.LongTensor(generate_binary_seq()) for _ in range(1000)]
 parity_li = [torch.LongTensor(compute_parity_seq(e)) for e in seq_li]
 
 # seq = generate_binary_seq()
@@ -54,12 +54,52 @@ parity_check = ParityCheck()
 sgd = optim.Adam(parity_check.parameters())
 criterion = nn.CrossEntropyLoss()
 
-for epoch in range(500):
+for epoch in range(20):
 
-    for i, seq in enumerate(seq_li[:1]):
+    running_loss = 0
+    n_info = 100
+    for i, seq in enumerate(seq_li):
         sgd.zero_grad()
         output = parity_check(seq)
         loss = criterion(output, parity_li[i])
+        running_loss += loss.item()
+        if i % n_info == n_info - 1 and (epoch == 0 or epoch % 10 == 9):
+            print(f'{epoch} {i}', running_loss / n_info)
+            running_loss = 0
+
         loss.backward()
-        print(f'{epoch} {i}', loss.item())
+
         sgd.step()
+
+random.seed()
+
+val_seq_li = [torch.LongTensor(generate_binary_seq()) for _ in range(100)]
+val_parity_li = [torch.LongTensor(compute_parity_seq(e)) for e in val_seq_li]
+
+
+def validate():
+    running_loss = 0
+    n_info = 10
+    n_correct = 0
+    for i, seq in enumerate(val_seq_li):
+        output = parity_check(seq)
+        loss = criterion(output, val_parity_li[i])
+        running_loss += loss.item()
+        if i % n_info == n_info - 1:
+            print(f'{i}', running_loss / n_info)
+            running_loss = 0
+        
+        n_correct += torch.sum(torch.argmax(output, 1) == val_parity_li[i]).item()
+    
+    print(n_correct / (len(val_parity_li) * len(val_parity_li[0])))
+
+
+validate()
+
+all_even = torch.LongTensor([2] * 40)
+output = parity_check(all_even)
+torch.argmax(output, 1)
+
+all_odd = torch.LongTensor([1] * 20)
+output = parity_check(all_odd)
+torch.argmax(output, 1)
